2ee191b35d4d6c1fc28276e72f80bacba4e8aefe
Yeshwant-22 <yeshwant.rajaraman@cdw.com>
2025-02-24 13:22:11 +0530

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -53,7 +53,7 @@ function handleInputChange(event) {
 function getSavedDashboardsAPI() {
     const savedDashboardsAPI = [
         {
-            "MAS Sandbox Development":"https://szw9nl20j5.execute-api.us-east-1.amazonaws.com/test/getdashboards",
+            "CDW Cloud MS":"https://11iu54aaq1.execute-api.us-east-1.amazonaws.com/csc-cloud/getdashboards",
         },
         {
             "MAS Sandbox Test1":"https://8vauowiu26.execute-api.us-east-1.amazonaws.com/test/getdashboards",
